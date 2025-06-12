9db3143dc8e22ed3f51f90232e61c1ab2d38632e
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-06-13 01:00:27 +0200

    test improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
 	<AppSidebar variant="inset" />
 	<Sidebar.Inset>
 		<SiteHeader />
+		<h1>Hello</h1>
 		<div class="flex flex-1 flex-col">
 			<div class="@container/main flex flex-1 flex-col gap-2">
 				<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -6,6 +6,8 @@ import Page from './+page.svelte';
 describe('/+page.svelte', () => {
 	test('should render h1', () => {
 		render(Page);
-		expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
+		const h1 = screen.getByRole('heading', { level: 1 })
+		expect(h1).toBeInTheDocument();
+		expect(h1).toContain('Hello')
 	});
 });
