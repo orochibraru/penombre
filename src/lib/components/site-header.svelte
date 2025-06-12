8e991a2806e8c77d6cd61198944e507c6352e070
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-06-12 12:58:08 +0200

    feat: auth pages

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { Separator } from "$lib/components/ui/separator/index.js";
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 </script>
 
 <header
-	class="h-(--header-height) group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) flex shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear"
+	class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)"
 >
 	<div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
 		<Sidebar.Trigger class="-ml-1" />
@@ -13,14 +13,12 @@
 		<h1 class="text-base font-medium">Documents</h1>
 		<div class="ml-auto flex items-center gap-2">
 			<Button
-				href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+				href="/auth/sign-in"
 				variant="ghost"
 				size="sm"
 				class="dark:text-foreground hidden sm:flex"
-				target="_blank"
-				rel="noopener noreferrer"
 			>
-				GitHub
+				Auth
 			</Button>
 		</div>
 	</div>
