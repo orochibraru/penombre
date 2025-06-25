78c77cf3fd095f8fa15164c04dfcac1fae83f85e
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-06-25 13:43:35 +0200

    feat: new build tool

--- scripts/build-migration.ts
+++ scripts/build-migration.ts
@@ -1,11 +1,11 @@
 import * as esbuild from 'esbuild';
 
 await esbuild.build({
-    entryPoints: ['scripts/migrate.ts'],
-    bundle: true,
-    platform: 'node',
-    target: 'node18',
-    outfile: 'dist/migrate.js',
-    format: 'esm',
-    external: ['pg-native'],
+	entryPoints: ['scripts/migrate.ts'],
+	bundle: true,
+	platform: 'node',
+	target: 'node18',
+	outfile: 'dist/migrate.js',
+	format: 'esm',
+	external: ['pg-native']
 });
