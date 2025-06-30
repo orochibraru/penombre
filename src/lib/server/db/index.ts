2a003b5d1e116d85e4eab39e4d343c0fb168c359
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-07-01 00:44:06 +0200

    jpp

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,16 +1,9 @@
-import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import * as schema from '$lib/server/db/schema/auth';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-export const dbUrl = env.DATABASE_URL;
-
-if (!dbUrl) {
-	if (!building) {
-		throw new Error('Missing database url');
-	}
-}
+export const dbUrl = env.DATABASE_URL ?? 'postgres://postgres:postgres@0.0.0.0:5432/opendrive';
 
 const client = postgres(dbUrl);
 
