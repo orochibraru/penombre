c8119059591ab376072cba4fff2c1730b8f01de4
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-10-22 14:06:11 +0200

    fix: formatting
    Former-commit-id: bf61bd2f4e322d1c76ad84219328a2183b4dcb7f

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,18 +2,21 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-    "*": ["pnpm run format"],
+    "packages/ui/**/*": [
+        "pnpm -C packages/ui run format",
+    ],
+    "packages/docs/**/*": [
+        "pnpm -C packages/docs run format",
+    ],
     "packages/ui/**/*.{ts,svelte}": [
         "pnpm -C packages/ui run lint:fix",
         "pnpm -C packages/ui run check",
         "pnpm -C packages/ui run circular",
-        "pnpm -C packages/ui run format",
     ],
     "packages/docs/**/*.{ts,svelte}": [
         "pnpm -C packages/docs run lint:fix",
         "pnpm -C packages/docs run check",
         "pnpm -C packages/docs run circular",
-        "pnpm -C packages/docs run format",
     ],
     "**/*.go": [
         "sh -c 'cd packages/api && go fmt'",
