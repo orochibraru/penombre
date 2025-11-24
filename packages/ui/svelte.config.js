dab72d2295438b10386ddcd3d37303adbdf8dd1d
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-11-24 14:24:48 +0100

    fix: svelte config for building
    Former-commit-id: 63e5d8ce89477762432492fd02566fbeabe1acd6

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -6,13 +6,27 @@ import packageJson from "./package.json" with { type: "json" };
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
 			fallback: "index.html",
-			precompress: false,
-			strict: true,
+			pages: "./dist",
+			error: "error.html",
 		}),
+		prerender: {
+			handleHttpError: ({ status, message }) => {
+				if (status > 300 && status < 307) {
+					return;
+				}
+
+				if (status === 404) {
+					return;
+				}
+
+				throw new Error(message);
+			},
+		},
 		version: {
 			name: packageJson.version,
 		},
