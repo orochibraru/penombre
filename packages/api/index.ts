9cf0185ddf55360b38ca2099ed5528bfb689990c
Nicolas Boyer <boyer63nicolas@gmail.com>
2025-11-28 16:51:15 +0100

    fix: origin
    Former-commit-id: 965d384192b4f06fb9441e6125d7ad282479eb1d

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -132,6 +132,7 @@ const server = new Api({
 					) {
 						return { skip: true, proceed: false };
 					}
+					process.env.ORIGIN = `http://${host}:${port}`;
 					const handler = await import("./frontend/handler");
 					const { fetch } = handler.getHandler();
 					const res = await fetch(request);
