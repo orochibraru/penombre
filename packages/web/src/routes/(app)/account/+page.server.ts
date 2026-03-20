f3f3b8bffa3e747cbe88c8f6fe9f6e06320d4881
Oro <boyer63nicolas@gmail.com>
2026-03-20 10:44:58 +0100

    wip :email change

--- packages/web/src/routes/(app)/account/+page.server.ts
+++ packages/web/src/routes/(app)/account/+page.server.ts
@@ -1,7 +1,11 @@
 import { fail } from "@sveltejs/kit";
+import { Logger } from "$lib/logger";
 import { auth } from "$lib/server/auth";
+import { getPenombreConfig } from "$lib/server/config";
 
 export const load = () => {};
+
+const logger = new Logger("account-page.server.ts");
 
 export const actions = {
 	updateAccount: async ({ request }) => {
@@ -13,8 +17,36 @@ export const actions = {
 			return fail(400, { error: "Name and email are required" });
 		}
 
+		const config = getPenombreConfig();
+
 		// Here you would typically update the user's account details in your database
-		console.log("Updating account with name:", name, "and email:", email);
+		logger.debug("Updating account with name:", name, "and email:", email);
+
+		const authRes = await auth.api.getSession({ headers: request.headers });
+		if (!authRes?.user) {
+			logger.error("No authenticated user found in session");
+			return fail(401, { error: "Unauthorized" });
+		}
+
+		if (email !== authRes.user.email) {
+			if (!config.smtp?.enabled) {
+				logger.error("Attempted email change without SMTP enabled");
+				return fail(400, {
+					error: "Email change is not allowed without SMTP enabled",
+				});
+			}
+			const emailRes = await auth.api.adminUpdateUser({
+				body: {
+					userId: authRes.user.id,
+					data: {
+						email: String(email),
+					},
+				},
+				headers: request.headers,
+			});
+			logger.error("Email change attempted, which is not allowed");
+			return fail(400, { error: "Email change is not allowed" });
+		}
 
 		try {
 			const res = await auth.api.updateUser({
@@ -25,12 +57,13 @@ export const actions = {
 			});
 
 			if (!res.status) {
+				logger.error("Failed to update account details:", res);
 				return fail(500, { error: "Failed to update account details" });
 			}
 
 			return { success: true };
 		} catch (error) {
-			console.error("Error updating account details:", error);
+			logger.error("Error updating account details:", error);
 			return fail(500, {
 				error: "An unexpected error occurred while updating account details",
 			});
