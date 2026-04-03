f40561512fbea749e6ac60151967804fde1797ce
Oro <boyer63nicolas@gmail.com>
2026-04-03 13:50:36 +0200

    chore: removed unused script

--- docker/garage-init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Initialize garage bucket
-
-if [ "$STORAGE_BACKEND" = "s3" ]; then
-  echo "Initializing garage bucket..."
-  aws --endpoint-url="$S3_ENDPOINT" s3api create-bucket --bucket "$S3_BUCKET" --region "$S3_REGION"
-fi
